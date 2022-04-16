f27435c1fec32c9d4ffe27b571f09638332e183e
tim.nebel@hhu.de <tim.nebel@hhu.de>
2022-04-16 12:34:49 +0200

    add ferris-says

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
+    let stdout = stdout();
+    let message1 = String::from("Hello Enterprise");
+    let width1 = message1.chars().count();
+    let message2 = String::from("Hello Voyager");
+    let width2 = message2.chars().count();
+    
+    let mut writer = BufWriter::new(stdout.lock());
+    println!("{:#?}, {:#?}", width2, width1);
+    println!("{:#?}", say(message2.as_bytes(), width2, &mut writer));
+    say(message1.as_bytes(), 100, &mut writer).unwrap();
     println!("Hello, world!");
 }
